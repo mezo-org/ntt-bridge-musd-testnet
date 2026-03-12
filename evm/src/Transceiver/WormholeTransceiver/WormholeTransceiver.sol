14813b397b54a4679fc0b490d776a267b8c14edb
Dymitr (Dmitry) Paremski <dymitr.paremski@thesis.co>
2026-03-12 14:15:24 +0100

    Adding deployment logs and deployment.json with updated addresses
    We had to deploy new WormholeTransceiver and then upgrade it. This
    commit adds deployment logs and deployment.json file with all the
    updated addresses.

--- evm/src/Transceiver/WormholeTransceiver/WormholeTransceiver.sol
+++ evm/src/Transceiver/WormholeTransceiver/WormholeTransceiver.sol
@@ -103,6 +103,12 @@ contract WormholeTransceiver is IWormholeTransceiver, WormholeTransceiverState {
 
     // ==================== Internal ========================================================
 
+    /// @dev Override _migrate to skip immutables check, since this version removes the
+    ///      wormholeRelayer immutable that existed in the previously deployed version.
+    function _migrate() internal override {
+        _setMigratesImmutables(true);
+    }
+
     function _quoteDeliveryPrice(
         uint16, /* targetChain */
         TransceiverStructs.TransceiverInstruction memory /* instruction */
